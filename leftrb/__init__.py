f5911f8714cbf3b49fb80b57dc07d0ce24c0eb4e
Peter Hillerström <peter.hillerstrom@gmail.com>
2013-01-22 20:18:33 +0200

    Made from leftrb import * only import LeftRB from leftrb.llrb module and not BinarySearchTree also.

--- leftrb/__init__.py
+++ leftrb/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python -u
 # encoding: utf-8
 
-from leftrb.bst import BinarySearchTree
 from leftrb.llrb import LeftRB
 
-__all__ = ['BinarySearchTree', 'LeftRB']
+__all__ = ['LeftRB']
