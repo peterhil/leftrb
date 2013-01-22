ba981e455f15d1e366bc8a3b1092d59706fca2fc
Peter Hillerström <peter.hillerstrom@gmail.com>
2013-01-22 19:06:10 +0200

    Separated the basic BinarySearchTree into parent class. Moved the rotatations and flip_colors into LeftRB class. Added a simple test.

--- llrb.py
+++ llrb.py
@@ -23,83 +23,28 @@ RED = True
 BLACK = False
 
 
-def rotate_left(h):
-    """
-    Left rotate (right link of h)
-
-           V         |          V <--left or right, red or black
-           |         |          |
-    out<--(x)   <<< LEFT       (h) <--in
-         // \        |         / \\  <--red  
-       (h)   3       |        1   (x)
-       / \           |            / \
-      1   2          |           2   3
-    """
-    x       = h.right
-    h.right = x.left
-    x.left  = h
-    x.color = h.color
-    h.color = RED
-    return x
-
-
-def rotate_right(h):
-    """
-    Right rotate (left link of h)
-
-           V         |          V <--left or right, red or black
-           |         |          |
-    in--> (h)     RIGHT >>>    (x)-->out
-         // \        |         / \\  <--red  
-       (x)   3       |        1   (h)
-       / \           |            / \
-      1   2          |           2   3
-    """
-    x       = h.left
-    h.left  = x.right
-    x.right = h
-    x.color = h.color
-    h.color = RED
-    return x
-
-
-def flip_colors(h):
-    """
-    Flip colors to split a 4-node
-    """
-    h.color = not h.color
-    h.left.color = not h.left.color
-    h.right.color = not h.right.color
-
-
 def is_red(h):
     """
     Is the node (h) red?
     """
-    return h is not None and h.color == RED
+    return isinstance(h, LeftRB.Node) and h.color == RED
 
 
-class LeftRB(object):
+class BinarySearchTree(object):
     """
-    Left-Leaning Red-Black (LLRB) is an implementation of
-    2–3 balanced binary search tree.
-
-    Ported to Python from code and description on
-    paper “Left-leaning Red-Black Trees” by Robert Sedgewick:
-    http://www.cs.princeton.edu/~rs/talks/LLRB/LLRB.pdf
+    Basic unbalanced (inefficient) search tree.
     """
     root = None
 
     class Node(object):
         """
-        LLRB tree node.
+        BST tree node.
         """
         left = right = None
 
         def __init__(self, key, val=None):
             self.key = key
             self.val = val
-            self.color = RED  # new nodes are always red
 
     def search(self, key):
         """
@@ -122,28 +67,132 @@ class LeftRB(object):
         self.root = self._insert(self.root, key, value)
         self.root.color = BLACK
 
-    @staticmethod
-    def _insert(h, key, value=None):
+    @classmethod
+    def _insert(cls, h, key, value=None):
         """
-        Private insert function for recursion.
+        Insert a node (h) node recursively.
         """
         if h is None:
-            return LeftRB.Node(key, value)
-
-        # Move this to the end to get 2-3 trees
-        if is_red(h.left) and is_red(h.right):
-            flip_colors(h)
+            return cls.Node(key, value)
 
         if h.key == key:
             h.val = value
         elif key < h.key:
-            h.left = LeftRB._insert(h.left, key, value);
+            h.left = cls._insert(h.left, key, value)
         else:
-            h.right = LeftRB._insert(h.right, key, value);
-
-        if is_red(h.right) and not is_red(h.left):
-            h = rotate_left(h)
-        if is_red(h.left) and is_red(h.left.left):
-            h = rotate_right(h)
+            h.right = cls._insert(h.right, key, value)
 
         return h
+
+
+class LeftRB(BinarySearchTree, object):
+    """
+    Left-Leaning Red-Black (LLRB) is an implementation of
+    2–3 balanced binary search tree.
+
+    Ported to Python from code and description on
+    paper “Left-leaning Red-Black Trees” by Robert Sedgewick:
+    http://www.cs.princeton.edu/~rs/talks/LLRB/LLRB.pdf
+    """
+    root = None
+
+    class Node(BinarySearchTree.Node, object):
+        """
+        LeftRB tree node.
+        """
+        def __init__(self, key, val=None):
+            super(self.__class__, self).__init__(key, val)
+            self.color = RED  # new nodes are always red
+
+    @classmethod
+    def _insert(cls, h, key, value=None):
+        if h is None:
+            return cls.Node(key, value)
+
+        # Move this to the end to get 2-3 trees
+        if is_red(h.left) and is_red(h.right):
+            cls._flip_colors(h)
+
+        h = super(cls, cls)._insert(h, key, value)
+
+        if is_red(h.right) and not is_red(h.left):
+            h = cls._rotate_left(h)
+        if is_red(h.left) and is_red(h.left.left):
+            h = cls._rotate_right(h)
+
+        return h
+
+    @staticmethod
+    def _flip_colors(h):
+        """
+        Flip colors to split a 4-node
+        """
+        h.color = not h.color
+        h.left.color = not h.left.color
+        h.right.color = not h.right.color
+
+    @staticmethod
+    def _rotate_left(h):
+        """
+        Left rotate (right link of h)
+
+               V         |          V <--left or right, red or black
+               |         |          |
+        out<--(x)   <<< LEFT       (h) <--in
+             // \        |         / \\  <--red  
+           (h)   3       |        1   (x)
+           / \           |            / \
+          1   2          |           2   3
+        """
+        x       = h.right
+        h.right = x.left
+        x.left  = h
+        x.color = h.color
+        h.color = RED
+        return x
+
+    @staticmethod
+    def _rotate_right(h):
+        """
+        Right rotate (left link of h)
+
+               V         |          V <--left or right, red or black
+               |         |          |
+        in--> (h)     RIGHT >>>    (x)-->out
+             // \        |         / \\  <--red  
+           (x)   3       |        1   (h)
+           / \           |            / \
+          1   2          |           2   3
+        """
+        x       = h.left
+        h.left  = x.right
+        x.right = h
+        x.color = h.color
+        h.color = RED
+        return x
+
+
+def test_random():
+    import random
+
+    allpass = True
+    for cls in [BinarySearchTree, LeftRB]:
+        print("Testing %s:" % cls.__name__),
+        t = cls()
+        r = random.sample(xrange(1000), 900)
+        for x in r:
+            t.insert(x)
+        res = [t.search(x) for x in r]
+        try:
+            assert r == res
+            print('PASS')
+        except AssertionError, e:
+            print("FAIL: lists are not equal!\n%s" % e)
+            allpass = False
+
+    print("All passed: %s" % allpass)
+    return allpass
+
+
+if __name__ == '__main__':
+    test_random()
